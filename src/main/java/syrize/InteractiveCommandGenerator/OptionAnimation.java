d9c32a50e5c79fb2b28193d5867d9bc679bbae9a
syrizelink <2532124404@qq.com>
2023-02-16 10:53:09 +0800

    依赖配置更新

--- src/main/java/syrize/InteractiveCommandGenerator/OptionAnimation.java
+++ src/main/java/syrize/InteractiveCommandGenerator/OptionAnimation.java
@@ -2,18 +2,22 @@ package syrize.InteractiveCommandGenerator;
 
 import java.util.List;
 
+/**
+ * @author Syrize
+ */
 public class OptionAnimation {
     static int num = 0;
     static String CSelected, CNotSelected;
-    public void switchingEffect(List<String> op, int Status){
+    public void switchingEffect(List<String> op, int status){
         CSelected = "◈";
         CNotSelected = "◇";
-        switch (Status){
-            case 0: {
+        switch (status){
+            default: {
                 while (num < op.size()){
                     System.out.println(op.get(num));
                     num++;
                 }
+                break;
             }
 
             case 1: {
@@ -21,6 +25,7 @@ public class OptionAnimation {
                     System.out.println(op.get(num));
                     num++;
                 }
+
             }
         }
     }
